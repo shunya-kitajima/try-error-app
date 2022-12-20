567cd7746da58f5848fef2f5a9d2c4356d7ee795
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 14:50:57 +0900

    import modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
