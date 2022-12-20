c3ae0c19ee5631df88c3fd6350160d5839aa8349
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 21:04:47 +0900

    add states and update functions

--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,36 @@
 import create from 'zustand'
 import { Session } from '@supabase/supabase-js'
+import { EditedDaily, EditedTry } from '../types'
 
 type State = {
   session: Session | null
   setSession: (payload: Session | null) => void
+  editedDaily: EditedDaily
+  updateEditedDaily: (payload: EditedDaily) => void
+  resetEditedDaily: () => void
+  editedTry: EditedTry
+  updateEditedTry: (payload: EditedTry) => void
+  resetEditedTry: () => void
 }
 
 const useStore = create<State>((set) => ({
   session: null,
   setSession: (payload) => set({ session: payload }),
+  editedDaily: { id: '', user_id: '' },
+  updateEditedDaily: (payload) =>
+    set({
+      editedDaily: {
+        id: payload.id,
+        user_id: payload.user_id,
+      },
+    }),
+  resetEditedDaily: () => set({ editedDaily: { id: '', user_id: '' } }),
+  editedTry: { id: '', try: '', result: '' },
+  updateEditedTry: (payload) =>
+    set({
+      editedTry: { id: payload.id, try: payload.try, result: payload.result },
+    }),
+  resetEditedTry: () => set({ editedTry: { id: '', try: '', result: '' } }),
 }))
 
 export default useStore
