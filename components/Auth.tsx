7f9b781f571563f584c717ace720938d122b7235
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 15:09:44 +0900

    create submitHandler

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -3,5 +3,29 @@ import { ShieldCheckIcon } from '@heroicons/react/24/solid'
 import { useMutateAuth } from '../hooks/useMutateAuth'
 
 export const Auth: React.FC = () => {
-  return <div>Auth</div>
+  const [isLogin, setIsLogin] = useState(true)
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loginMutation,
+    registerMutation,
+  } = useMutateAuth()
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (isLogin) {
+      loginMutation.mutate()
+    } else {
+      registerMutation.mutate()
+    }
+  }
+
+  return (
+    <>
+      <ShieldCheckIcon className="mb-8 h-12 w-12 text-blue-500" />
+      <form></form>
+    </>
+  )
 }
