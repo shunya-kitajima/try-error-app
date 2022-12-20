5ad761cd1bca7fbd9c004242a13a029384099aef
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 14:53:24 +0900

    rap for component by queryclientprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,23 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      suspense: false,
+    },
+  },
+})
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
