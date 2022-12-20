88c0c74cb2dc0c7722c0dc24ff28823ecabc1d8e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 20:54:54 +0900

    create types

---
+++ types/index.ts
@@ -0,0 +1,29 @@
+export type Daily = {
+  id: string
+  created_at: string
+  updated_at: string
+  user_id: string
+}
+
+export type EditedDaily = {
+  id: string
+  user_id: string
+}
+
+export type Try = {
+  id: string
+  created_at: string
+  updated_at: string
+  user_id: string
+  daily_id: string
+  try: string
+  result: string
+}
+
+export type EditedTry = {
+  id: string
+  user_id: string
+  daily_id: string
+  try: string
+  result: string
+}
