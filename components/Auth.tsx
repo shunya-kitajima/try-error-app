47fd50c8355ea75cbf9c408abf3fdeff397c23ea
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 15:05:51 +0900

    add file and import modules

---
+++ components/Auth.tsx
@@ -0,0 +1,7 @@
+import React, { useState, FormEvent } from 'react'
+import { ShieldCheckIcon } from '@heroicons/react/24/solid'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
+export const Auth: React.FC = () => {
+  return <div>Auth</div>
+}
