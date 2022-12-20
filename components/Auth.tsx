d2ad3fc43f7b8ed573c89e205d55e283fb009ec7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 15:16:33 +0900

    create login form

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -25,7 +25,46 @@ export const Auth: React.FC = () => {
   return (
     <>
       <ShieldCheckIcon className="mb-8 h-12 w-12 text-blue-500" />
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            required
+            className="my-2 rounded border border-gray-300 px-3 py-2 text-sm focus:outline-none"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            required
+            className="my-2 rounded border border-gray-300 px-3 py-2 text-sm focus:outline-none"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+          />
+        </div>
+        <div className="my-6 flex items-center justify-center text-sm">
+          <span
+            className="cursor-pointer font-medium hover:text-indigo-500"
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            Change mode ?
+          </span>
+        </div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
+        >
+          {isLogin ? 'Login' : 'Register'}
+        </button>
+      </form>
     </>
   )
 }
