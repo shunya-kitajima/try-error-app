a7ed88b024011e608a85f0113174dd10d0a2d206
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-20 16:49:57 +0900

    create and export store

---
+++ store/indx.ts
@@ -0,0 +1,14 @@
+import create from 'zustand/react'
+import { Session } from '@supabase/supabase-js'
+
+type State = {
+  session: Session | null
+  setSession: (payload: Session | null) => void
+}
+
+const useStore = create<State>((set) => ({
+  session: null,
+  setSession: (payload) => set({ session: payload }),
+}))
+
+export default useStore
