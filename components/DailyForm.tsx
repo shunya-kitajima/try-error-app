1be0e9c6888d3d58a9b568033f288d85ea89c637
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-21 15:11:17 +0900

    display input and textarea

--- components/DailyForm.tsx
+++ components/DailyForm.tsx
@@ -4,20 +4,35 @@ import Link from 'next/link'
 const DailyForm: React.FC = () => {
   return (
     <>
-      <Link href={'/'} prefetch={false}>
+      <div className="flex items-center justify-around">
+        <Link href={'/'} prefetch={false}>
+          <button
+            type="button"
+            className="flex w-full justify-center rounded-md bg-slate-50 px-4 py-2 text-sm text-indigo-600"
+          >
+            cancel
+          </button>
+        </Link>
         <button
           type="button"
-          className="flex w-full justify-center rounded-md bg-slate-50 px-4 py-2 text-sm text-indigo-600"
+          className="w-25 flex justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
         >
-          cancel
+          submit
         </button>
-      </Link>
-      <button
-        type="button"
-        className="w-25 flex justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
-      >
-        submit
-      </button>
+      </div>
+      <div className="flex items-center justify-around">
+        <input
+          type="text"
+          className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+          placeholder="try"
+        />
+        <textarea
+          cols={50}
+          rows={10}
+          className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+          placeholder="content"
+        />
+      </div>
     </>
   )
 }
