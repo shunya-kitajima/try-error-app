02f8c9156d0d64fc42f0406bee687ec4403fd272
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-21 10:31:52 +0900

    display DailyForm component

--- pages/daily.tsx
+++ pages/daily.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
+import DailyForm from '../components/DailyForm'
 
 const daily: NextPage = () => {
-  return <div>daily</div>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <DailyForm />
+    </div>
+  )
 }
 
 export default daily
