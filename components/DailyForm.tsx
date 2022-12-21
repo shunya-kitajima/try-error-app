1a15aa9d92cf14e62652e28d0d319f04033c062e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-21 10:37:39 +0900

    display button link for indexpage

--- components/DailyForm.tsx
+++ components/DailyForm.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 const DailyForm: React.FC = () => {
-  return <div></div>
+  return (
+    <>
+      <Link href={'/'} prefetch={false}>
+        <button
+          type="button"
+          className="flex w-full justify-center rounded-md bg-slate-50 px-4 py-2 text-sm text-indigo-600"
+        >
+          cancel
+        </button>
+      </Link>
+    </>
+  )
 }
 
 export default DailyForm
