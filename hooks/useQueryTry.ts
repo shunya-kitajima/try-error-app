dca77551e5c141d14165b90f0d25cd4d7ed4bf17
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-22 17:16:40 +0900

    add condition

--- hooks/useQueryTry.ts
+++ hooks/useQueryTry.ts
@@ -2,11 +2,12 @@ import { useQuery } from '@tanstack/react-query'
 import { supabase } from '../utils/supabase'
 import { Try } from '../types'
 
-const useQueryTry = () => {
+const useQueryTry = (dailyId: string) => {
   const getTries = async () => {
     const { data, error } = await supabase
       .from('tries')
       .select('*')
+      .eq('daily_id', dailyId)
       .order('created_at', { ascending: true })
     if (error) throw new Error(error.message)
     return data
