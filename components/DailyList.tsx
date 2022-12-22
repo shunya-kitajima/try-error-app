97fe279d5fda5c7b1079f792233bd01c4c8951d6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-22 17:48:08 +0900

    display dailies

---
+++ components/DailyList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import useStore from '../store'
+import { useQueryDaily } from '../hooks/useQueryDaily'
+
+export const DailyList: React.FC = () => {
+  const session = useStore((state) => state.session)
+  const { data: dailies } = useQueryDaily(session?.user?.id!)
+
+  return (
+    <>
+      <p>Daily</p>
+      <ul>
+        {dailies?.map((daily) => (
+          <li key={daily.id}>{daily.id}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
