4fcb56ebd2ae0198a5dc40ead5ad9dcbe2af4d65
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-22 09:36:21 +0900

    create updateTryMutation

--- hooks/useMutateTry.ts
+++ hooks/useMutateTry.ts
@@ -15,9 +15,37 @@ const useMutateTry = () => {
     },
     {
       onSuccess: (res) => {
-        let previousTryies = queryClient.getQueryData<Try[]>(['tries'])
-        if (!previousTryies) previousTryies = []
-        queryClient.setQueryData(['tryies'], [...previousTryies, res[0]])
+        let previousTries = queryClient.getQueryData<Try[]>(['tries'])
+        if (!previousTries) previousTries = []
+        queryClient.setQueryData(['tries'], [...previousTries, res[0]])
+        resetEditedTry()
+      },
+      onError: (err: any) => {
+        resetEditedTry()
+        throw new Error(err.message)
+      },
+    }
+  )
+
+  const updateTryMutation = useMutation(
+    async (paramTry: EditedTry) => {
+      const { data, error } = await supabase
+        .from('tries')
+        .update({ try: paramTry.try, result: paramTry.result })
+        .eq('id', paramTry.id)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: (res, variables) => {
+        let previousTries = queryClient.getQueryData<Try[]>(['tries'])
+        if (!previousTries) previousTries = []
+        queryClient.setQueryData(
+          ['tries'],
+          previousTries.map((paramTry) =>
+            paramTry.id === variables.id ? res[0] : paramTry
+          )
+        )
         resetEditedTry()
       },
       onError: (err: any) => {
