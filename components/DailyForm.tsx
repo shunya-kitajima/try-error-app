63f5ac9459e31aaefbff72690f1c0149ca084c16
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-22 09:01:55 +0900

    set states

--- components/DailyForm.tsx
+++ components/DailyForm.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import { supabase } from '../utils/supabase'
+import useStore from '../store'
+import useMutateDaily from '../hooks/useMutateDaily'
 
 const DailyForm: React.FC = () => {
+  const { editedDaily } = useStore()
+  const { editedTry } = useStore()
+  const updateEditedTry = useStore((state) => state.updateEditedTry)
+
   return (
     <>
       <div className="flex items-center justify-around">
@@ -25,18 +32,26 @@ const DailyForm: React.FC = () => {
           type="text"
           className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
           placeholder="try"
+          value={editedTry.try}
+          onChange={(e) =>
+            updateEditedTry({ ...editedTry, try: e.target.value })
+          }
         />
         <textarea
           cols={40}
           rows={10}
           className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
           placeholder="result"
+          value={editedTry.result}
+          onChange={(e) =>
+            updateEditedTry({ ...editedTry, result: e.target.value })
+          }
         />
         <button
           type="button"
           className="w-25 flex justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
         >
-          create
+          {editedTry.id ? 'update' : 'create'}
         </button>
       </div>
     </>
