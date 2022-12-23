30f1f24c82401943d65f4a422856d595d0db91f0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-23 09:38:21 +0900

    delete waste line

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,12 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
-import { supabase } from '../utils/supabase'
-import useStore from '../store'
 import { Layout } from '../components/Layout'
 import { Auth } from '../components/Auth'
-import { DashBoard } from '../components/DashBoard'
 
 const Home: NextPage = () => {
-  const session = useStore((state) => state.session)
-  const setSession = useStore((state) => state.setSession)
-
-  useEffect(() => {
-    const defaultSession = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession()
-      setSession(session)
-    }
-    defaultSession()
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [setSession])
-
   return (
-    <Layout title="Try and Error">{!session ? <Auth /> : <DashBoard />}</Layout>
+    <Layout title="Try and Error">
+      <Auth />
+    </Layout>
   )
 }
 
