4b2e94293ba31f62c0969ac1b0b843fb43edbe37
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-26 13:29:43 +0900

    delete when click trashicon

--- components/DailyItem.tsx
+++ components/DailyItem.tsx
@@ -11,12 +11,17 @@ type Props = {
 }
 
 const DailyItem: React.FC<Props> = ({ id, user_id, year, month, date }) => {
+  const { deleteDailyMutation } = useMutateDaily()
+
   return (
     <li className="my-3 flex items-center justify-between">
       <div className="flex">{`${year}/${month}/${date}`}</div>
       <div className="flex">
         <PencilIcon className="mx-1 h-5 w-5 cursor-pointer text-blue-500" />
-        <TrashIcon className="h-5 w-5 cursor-pointer text-blue-500" />
+        <TrashIcon
+          className="h-5 w-5 cursor-pointer text-blue-500"
+          onClick={() => deleteDailyMutation.mutate(id)}
+        />
       </div>
     </li>
   )
