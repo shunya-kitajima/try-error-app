4077cbd846cfeff7a923d9a92fc8090bff51e06a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-26 16:13:27 +0900

    display DailyForm and TryForm

---
+++ components/Daily.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DailyForm } from './DailyForm'
+import { TryForm } from './TryForm'
+
+const Daily: React.FC = () => {
+  return (
+    <>
+      <DailyForm />
+      <TryForm />
+    </>
+  )
+}
+
+export default Daily
