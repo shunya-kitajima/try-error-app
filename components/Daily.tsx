fa15968620bd0b90bd2c92d5b00f276344c4c253
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-27 16:54:42 +0900

    change for export stateaction

--- components/Daily.tsx
+++ components/Daily.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DailyForm } from './DailyForm'
 import { TryForm } from './TryForm'
+import { EditedTry } from '../types'
 
 export const Daily: React.FC = () => {
+  const [editedTry, setEditedTry] = useState<EditedTry>({
+    id: '',
+    user_id: '',
+    daily_id: '',
+    try: '',
+    result: '',
+  })
+
   return (
     <>
       <DailyForm />
-      <TryForm />
+      <TryForm editedTry={editedTry} setEditedTry={setEditedTry} />
     </>
   )
 }
