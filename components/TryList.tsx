b65ea123d2415435b90a0f890211da41c8d4ee23
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-27 17:12:28 +0900

    dispplay TryItem

---
+++ components/TryList.tsx
@@ -0,0 +1,29 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import { useQueryTry } from '../hooks/useQueryTry'
+import { TryItem } from './TryItem'
+import { EditedTry } from '../types'
+
+type Props = {
+  daily_id: string
+  setEditedTry: Dispatch<SetStateAction<EditedTry>>
+}
+
+export const TryList: React.FC<Props> = ({ daily_id, setEditedTry }) => {
+  const { data: tries } = useQueryTry(daily_id)
+
+  return (
+    <>
+      <ul>
+        {tries?.map((paramTry) => (
+          <TryItem
+            key={paramTry.id}
+            id={paramTry.id}
+            paramTry={paramTry.try}
+            result={paramTry.result}
+            setEditedTry={setEditedTry}
+          />
+        ))}
+      </ul>
+    </>
+  )
+}
