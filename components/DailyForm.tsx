79b04b9aa95980b2609153d0faaf544c28a8b9d0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-29 00:48:56 +0900

    change for using isCreate

--- components/DailyForm.tsx
+++ components/DailyForm.tsx
@@ -1,5 +1,6 @@
 import React, { MouseEvent } from 'react'
 import { useRouter } from 'next/router'
+import useStore from '../store'
 import { useMutateDaily } from '../hooks/useMutateDaily'
 import { EditedDaily } from '../types'
 
@@ -9,6 +10,7 @@ type Props = {
 
 export const DailyForm: React.FC<Props> = ({ editedDaily }) => {
   const router = useRouter()
+  const isCreate = useStore((state) => state.isCreate)
   const { deleteDailyMutation } = useMutateDaily()
 
   const deleteDailyHandler = async (e: MouseEvent<HTMLButtonElement>) => {
@@ -20,19 +22,21 @@ export const DailyForm: React.FC<Props> = ({ editedDaily }) => {
   return (
     <>
       <div className="flex items-center justify-around">
-        <button
-          type="button"
-          className="w-25 flex justify-center rounded-md bg-slate-50 px-4 py-2 text-sm text-indigo-600"
-          onClick={(e) => deleteDailyHandler(e)}
-        >
-          cancel
-        </button>
+        {isCreate === 'create' && (
+          <button
+            type="button"
+            className="w-25 flex justify-center rounded-md bg-slate-50 px-4 py-2 text-sm text-indigo-600"
+            onClick={(e) => deleteDailyHandler(e)}
+          >
+            cancel
+          </button>
+        )}
         <button
           type="button"
           className="w-25 flex justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
           onClick={() => router.push('/')}
         >
-          back to index
+          {isCreate === 'create' ? 'create' : 'back to index'}
         </button>
       </div>
     </>
