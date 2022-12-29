de43888855cff9fe493ecb211ce5bf643f1c91a3
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-29 09:46:49 +0900

    create spinner

---
+++ components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Spinner: React.FC = () => {
+  return (
+    <div className="my-5 h-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent"></div>
+  )
+}
