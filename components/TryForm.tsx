a893e4b584af22ec9332853205e5b4bb8e164c76
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-30 12:00:11 +0900

    change for not using state

--- components/TryForm.tsx
+++ components/TryForm.tsx
@@ -1,49 +1,33 @@
-import React, { Dispatch, SetStateAction, MouseEvent } from 'react'
+import React, { FormEvent } from 'react'
+import useStore from '../store'
 import { supabase } from '../utils/supabase'
 import { useMutateTry } from '../hooks/useMutateTry'
 import { Spinner } from './Spinner'
-import { EditedDaily, EditedTry } from '../types'
 
 type Props = {
-  editedDaily: EditedDaily
-  editedTry: EditedTry
-  setEditedTry: Dispatch<SetStateAction<EditedTry>>
+  daily_id: string
 }
 
-export const TryForm: React.FC<Props> = ({
-  editedDaily,
-  editedTry,
-  setEditedTry,
-}) => {
+export const TryForm: React.FC<Props> = ({ daily_id }) => {
+  const { editedTry } = useStore()
+  const { updateEditedTry } = useStore()
   const { createTryMutation, updateTryMutation } = useMutateTry()
 
-  const tryHandleSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
+  const tryHandleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (editedTry.id) {
       await updateTryMutation.mutateAsync({
         id: editedTry.id,
-        daily_id: editedDaily.id,
+        daily_id: daily_id,
         try: editedTry.try,
         result: editedTry.result,
-      })
-      setEditedTry({
-        id: '',
-        daily_id: '',
-        try: '',
-        result: '',
       })
     } else {
       await createTryMutation.mutateAsync({
         user_id: supabase.auth.user()?.id!,
-        daily_id: editedDaily.id,
+        daily_id: daily_id,
         try: editedTry.try,
         result: editedTry.result,
-      })
-      setEditedTry({
-        id: '',
-        daily_id: '',
-        try: '',
-        result: '',
       })
     }
   }
@@ -52,33 +36,39 @@ export const TryForm: React.FC<Props> = ({
     return <Spinner />
 
   return (
-    <>
-      <div className="flex items-center justify-around">
+    <form onSubmit={tryHandleSubmit}>
+      <div>
         <input
           type="text"
           className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
           placeholder="try"
           value={editedTry.try}
-          onChange={(e) => setEditedTry({ ...editedTry, try: e.target.value })}
+          onChange={(e) =>
+            updateEditedTry({ ...editedTry, try: e.target.value })
+          }
         />
+      </div>
+      <div>
         <textarea
-          cols={40}
+          cols={50}
           rows={10}
           className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
           placeholder="result"
           value={editedTry.result}
           onChange={(e) =>
-            setEditedTry({ ...editedTry, result: e.target.value })
+            updateEditedTry({ ...editedTry, result: e.target.value })
           }
         />
+      </div>
+      <div className="my-2 flex justify-center">
         <button
-          type="button"
-          className="w-25 flex justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
+          type="submit"
+          className="ml-2 rounded bg-indigo-600 px-3 py-2 text-sm font-medium text-white hover:bg-indigo-700"
           onClick={(e) => tryHandleSubmit(e)}
         >
           {editedTry.id ? 'update' : 'create'}
         </button>
       </div>
-    </>
+    </form>
   )
 }
