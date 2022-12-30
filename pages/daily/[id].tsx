4dd53896d9fe3a53e9b72e369283deaf43321d55
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-31 01:16:57 +0900

    modify

--- pages/daily/[id].tsx
+++ pages/daily/[id].tsx
@@ -8,7 +8,7 @@ import { TryItem } from '../../components/TryItem'
 import { Daily } from '../../types'
 
 const getAllDailyIds = async () => {
-  const { data: ids } = await supabase.from('dalies').select('id')
+  const { data: ids } = await supabase.from('dailies').select('id')
   return ids!.map((id) => {
     return {
       params: {
