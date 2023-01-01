18610d8b79f8eea3af1993f0a58e6510523f35d7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-01 15:23:56 +0900

    modify

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,3 @@
-export type Try = {
-  id: string
-  created_at: string
-  user_id: string
-  daily_id: string
-  try: string
-  result: string
-}
-
 export type Daily = {
   id: string
   created_at: string
@@ -14,7 +5,15 @@ export type Daily = {
   year: string
   month: string
   date: string
-  tries: Try[]
+}
+
+export type Try = {
+  id: string
+  created_at: string
+  user_id: string
+  daily_id: string
+  try: string
+  result: string
 }
 
 export type EditedDaily = Omit<Daily, 'created_at' | 'user_id' | 'tries'>
