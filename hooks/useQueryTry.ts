5b1af096246c542809febe11caeef686506ce75a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-01 15:30:21 +0900

    add file and create query

---
+++ hooks/useQueryTry.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import { supabase } from '../utils/supabase'
+import { Try } from '../types'
+
+export const useQueryTry = (dailyId: string) => {
+  const getTries = async () => {
+    const { data, error } = await supabase
+      .from('tries')
+      .select('*')
+      .eq('daily_id', dailyId)
+      .order('created_at', { ascending: true })
+    if (error) throw new Error(error.message)
+    return data
+  }
+
+  return useQuery<Try[], Error>({
+    queryKey: ['tries'],
+    queryFn: getTries,
+    staleTime: Infinity,
+  })
+}
