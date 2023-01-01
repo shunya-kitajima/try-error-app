8f92f7b4622c1c9c3b7495c89258d0b34785f94c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-01 16:03:42 +0900

    modify

--- hooks/useMutateDaily.ts
+++ hooks/useMutateDaily.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { supabase } from '../utils/supabase'
 import { Daily, EditedDaily } from '../types'
 
