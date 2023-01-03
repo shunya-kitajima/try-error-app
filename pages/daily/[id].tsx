fef98f491c081b2c90766479033dd53bc2be5fd5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-03 10:16:27 +0900

    display for lapped component

--- pages/daily/[id].tsx
+++ pages/daily/[id].tsx
@@ -4,8 +4,8 @@ import { useQueryClient } from '@tanstack/react-query'
 import { ChevronDoubleLeftIcon } from '@heroicons/react/24/solid'
 import useStore from '../../store'
 import { Layout } from '../../components/Layout'
-import { TryForm } from '../../components/TryForm'
-import { TryList } from '../../components/TryList'
+import { TryFormMemo } from '../../components/TryForm'
+import { TryListMemo } from '../../components/TryList'
 import { Daily } from '../../types'
 
 const DailyPage: NextPage = () => {
@@ -21,8 +21,8 @@ const DailyPage: NextPage = () => {
       {targetDaily !== undefined ? (
         <>
           <p className="text-3xl font-semibold text-blue-500">{`${targetDaily[0].year}/${targetDaily[0].month}/${targetDaily[0].date}`}</p>
-          <TryForm daily_id={targetDaily[0].id} />
-          <TryList daily_id={targetDaily[0].id} />
+          <TryFormMemo daily_id={targetDaily[0].id} />
+          <TryListMemo daily_id={targetDaily[0].id} />
         </>
       ) : (
         <p className="text-3xl font-semibold text-blue-500">not found</p>
