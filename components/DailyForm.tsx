ecef7a65a5b5d94370829b5b154519040c13d382
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-03 18:55:28 +0900

    create take over prev daily

--- components/DailyForm.tsx
+++ components/DailyForm.tsx
@@ -2,11 +2,13 @@ import React, { MouseEvent } from 'react'
 import { useQueryClient } from '@tanstack/react-query'
 import { supabase } from '../utils/supabase'
 import { useMutateDaily } from '../hooks/useMutateDaily'
+import { useMutateTry } from '../hooks/useMutateTry'
 import { Spinner } from './Spinner'
 import { Daily } from '../types'
 
 export const DailyForm: React.FC = () => {
   const { createDailyMutation } = useMutateDaily()
+  const { createTryMultipleMutation } = useMutateTry()
   const queryclient = useQueryClient()
 
   const addDailyHandler = async (e: MouseEvent<HTMLButtonElement>) => {
@@ -31,7 +33,51 @@ export const DailyForm: React.FC = () => {
     })
   }
 
-  if (createDailyMutation.isLoading || createDailyMutation.isError)
+  const takeOverPrevDailyHandler = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = now.getMonth() + 1
+    const date = now.getDate()
+    const ymd = `${year}/${month}/${date}`
+    let previousDailies = queryclient.getQueryData<Daily[]>(['dailies'])
+    let todayDaily = previousDailies?.filter((daily) => daily.ymd === ymd)
+    if (todayDaily?.length !== 0) {
+      alert('今日の分は既に作成されています')
+      return
+    }
+    const prevDaily = previousDailies?.filter(
+      (daily) => daily.ymd === `${year}/${month}/${date - 1}`
+    )
+    if (prevDaily?.length === 0) {
+      alert('前日の分がありません')
+      return
+    }
+    await createDailyMutation.mutateAsync({
+      user_id: supabase.auth.user()?.id!,
+      year: String(year),
+      month: String(month),
+      date: String(date),
+      ymd: ymd,
+    })
+    previousDailies = queryclient.getQueryData<Daily[]>(['dailies'])
+    todayDaily = previousDailies?.filter((daily) => daily.ymd === ymd)
+    const { data: prevTries } = await supabase
+      .from('tries')
+      .select('*')
+      .eq('daily_id', prevDaily !== undefined ? prevDaily[0].id : '')
+    const createData = prevTries?.map((paramTry) => {
+      return {
+        user_id: supabase.auth.user()?.id!,
+        daily_id: todayDaily !== undefined ? todayDaily[0].id : '',
+        try: paramTry.try,
+        result: paramTry.result,
+      }
+    })
+    await createTryMultipleMutation.mutateAsync(createData!)
+  }
+
+  if (createDailyMutation.isLoading || createTryMultipleMutation.isLoading)
     return <Spinner />
 
   return (
@@ -47,6 +93,7 @@ export const DailyForm: React.FC = () => {
         <button
           type="button"
           className="w-25 rounded-md bg-indigo-600 px-4 py-2 text-sm text-white"
+          onClick={(e) => takeOverPrevDailyHandler(e)}
         >
           take over previous Dayily
         </button>
