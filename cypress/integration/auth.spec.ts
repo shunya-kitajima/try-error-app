cbea740308e6374aa10101c2e611eac6f7971959
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-05 00:12:35 +0900

    navigate to Auth when logout clicked

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -9,5 +9,14 @@ describe('Authentication', () => {
     cy.get('[type="submit"]').click()
     cy.get('[data-testid="logout"]').should('be.visible')
   })
+  it('Shall navigate to Auth when logout clicked', () => {
+    cy.get('input[placeholder="Email"]').type('user1@test.com')
+    cy.get('input[placeholder="Password"]').type('password')
+    cy.get('[type="submit"]').click()
+    cy.get('[data-testid="logout"]').should('be.visible')
+    cy.get('[data-testid="logout"]').click()
+    cy.get('input[placeholder="Email"]').should('be.visible')
+    cy.get('input[placeholder="Password"]').should('be.visible')
+  })
 })
 export {}
