bb01515bf1d3d4a8f7a2e10b2654ab00b4b0971d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-05 00:08:13 +0900

    navigate to dailies page when login successfully

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -3,5 +3,11 @@ describe('Authentication', () => {
   beforeEach(() => {
     cy.visit('/')
   })
+  it('Shall navigate to dailies page when login successfully', () => {
+    cy.get('input[placeholder="Email"]').type('user1@test.com')
+    cy.get('input[placeholder="Password"]').type('password')
+    cy.get('[type="submit"]').click()
+    cy.get('[data-testid="logout"]').should('be.visible')
+  })
 })
 export {}
