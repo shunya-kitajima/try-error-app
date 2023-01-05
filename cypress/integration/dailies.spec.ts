2ba7e3629f9a26fea700b3993c68130859aaa26b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-05 13:45:37 +0900

    daily create and delete fine

---
+++ cypress/integration/dailies.spec.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+describe('dailies', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type('user1@test.com')
+    cy.get('input[placeholder="Password"]').type('password')
+    cy.get('[type="submit"]').click()
+    cy.get('[data-testid="logout"]').should('be.visible')
+  })
+  it('Shall daily create and delete fine', () => {
+    cy.get('[data-testid="addDaily"]').click()
+    cy.get('[data-testid="ul-daily"]').children().should('have.length', 5)
+    cy.get('[data-testid="ul-daily"]')
+      .children()
+      .should('have.text', '2023/1/5')
+    cy.get('[data-testid="trash-daily-2023/1/5"]').click()
+    cy.get('[data-testid="ul-daily"]').children().should('have.length', 4)
+    cy.get('[data-testid="ul-daily"]')
+      .children()
+      .should('not.have.text', '2023/1/5')
+  })
+})
+export {}
